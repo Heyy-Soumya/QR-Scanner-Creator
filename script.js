cf6d4dcfa2f3d757a2bd698fb33cf123a6c28888
Heyy-Soumya <136678689+Heyy-Soumya@users.noreply.github.com>
2024-07-30 20:39:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,32 @@ function domReady(fn) {
 }
 
 domReady(function () {
-
+    // Function to check if a string is a valid URL
+    function isValidUrl(string) {
+        try {
+            new URL(string);
+            return true;
+        } catch (_) {
+            return false;  
+        }
+    }
     // If a QR code is found
     function onScanSuccess(decodeText, decodeResult) {
         alert("Your QR code is: " + decodeText + "\nResult: " + JSON.stringify(decodeResult));
+
+        if (isValidUrl(decodeText)) {
+            // Redirect to the URL
+            window.location.href = decodeText;
+        }
     }
 
     let htmlscanner = new Html5QrcodeScanner(
         "my-qr-reader",
         { fps: 10, qrbos: 250 }
     );
-    htmlscanner.render(onScanSuccess);
+    htmlscanner.render(onScanSuccess, onScanError);
+    function onScanError(errorMessage) {
+        // handle scan error
+        console.error(`QR Code scan error: ${ERROR}`);
+    }
 })
